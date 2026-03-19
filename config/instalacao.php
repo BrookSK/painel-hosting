933a4ae4df47dc742a54c5deb458a441b2bff2e4
Lucas Vacari <lucasrvacari99@gmail.com>
2026-03-19 02:10:36 -0300

    feat: add installation configuration file with database connection settings

---
+++ config/instalacao.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+return [
+    'banco' => [
+        'driver' => 'mysql',
+        'host' => '127.0.0.1',
+        'porta' => 3306,
+        'database' => 'lrvcloud',
+        'usuario' => 'lrvcloud',
+        'senha' => 'sMUSQ6^+n5b2.',
+        'charset' => 'utf8mb4',
+    ],
+];
